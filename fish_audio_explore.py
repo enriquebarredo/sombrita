46c4cf0b296bf5aa4dc71874d109626bdab3e822
enriquebarredo <49217731+enriquebarredo@users.noreply.github.com>
2026-09-21 22:05:04 -0600

    Refactor extraction of Fish Audio voice model data
    Refactored the extraction of the items from Fish Audio's 'model?' API
    response for easier reuse--in case I want to extract other keys from
    the JSON.
    
    Changes to be committed:
    	modified:   fish_audio_explore.py

--- fish_audio_explore.py
+++ fish_audio_explore.py
@@ -7,7 +7,7 @@ domain = "https://api.fish.audio/model?"
 paging = "page_size=20&page_number=1"
 sort = "sort_by=score" # "sort_by=task_count" "sort_by=created_at"
 author = "author_id=d8b0991f96b44e489422ca2ddf0bd31d"  # official fish audio. it's the one with the copyright? 'no-risk!' logo.
-lang = "language=zh"  #fish.audio/model? does not have a dictionary entry for regional variants. no "zh-tw", no "yue-hk" just "zh". some have it under tags. some.
+lang = "language=ru"  #fish.audio/model? does not have a dictionary entry for regional variants. no "zh-tw", no "yue-hk" just "zh". some have it under tags. some.
 lang2 = "language=en"  #but you can look up voices with expressed support for multiple languages
 
 url_options = [paging, sort, author, lang]
@@ -39,6 +39,14 @@ for u in range(response_dict["total"]):
     voice["description"] = response_dict["items"][u]["description"]
     voice["tags"] = response_dict["items"][u]["tags"]
     voice["samples"] = response_dict["items"][u]["samples"]
+    list_of_voices += [voice]
+
+interesting_keys = ["_id", "title", "description", "tags", "samples"]
+list_of_voices = []
+for u in range(response_dict["total"]):
+    voice = {} # every voice is a dictionary
+    for v in range(len(interesting_keys)):
+        voice[interesting_keys[v]] = response_dict["items"][u][interesting_keys[v]]
     list_of_voices += [voice]
 
 with open(filename, "w") as file:
